b8d17aa506ba398f345bdd3b26893daca9dc7b98
Chung Chen-Kuan Zarek <ChungCK@kimeng.com>
2023-03-06 16:26:20 +0800

    test wording 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>MayBank TEST!!!</p>
+        <p>MayBank github page test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
